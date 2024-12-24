5133facb126085162ccfaa1523dde1095b8d2613
THIANESH G <thianesh2003@gmail.com>
2024-12-24 13:23:12 +0530

    home page and calculator

---
+++ cli-calculator/app.js
@@ -0,0 +1,18 @@
+const argvs = process.argv;
+const argv = argvs.slice(4);
+const operation = argv[5];
+const operator1 = parseInt(argv[4]);
+const operator2 = parseInt(argv[6]);
+
+if (operation === 'plus') {
+    console.log(operation + ' is ' + (operator1 + operator2));
+}
+if (operation === 'minus') {
+    console.log(operation + ' is ' + (operator1 - operator2));
+}
+if (operation === 'multiplied') {
+    console.log(operation + ' is ' + (operator1 * operator2));
+}
+if (operation === 'divided') {
+    console.log(operation + ' is ' + (operator1 / operator2));
+}
